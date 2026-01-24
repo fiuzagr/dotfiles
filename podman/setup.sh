efa85a6d0745d85748269caabb559e8c26095eac
Fiuza <fiuzagr@gmail.com>
2026-01-24 16:11:30 -0300

    Update podman for macOS

--- podman/setup.sh
+++ podman/setup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
 
-sudo apt install -y uidmap
+if is_macos; then
+  log 'Installing OrbStack for container management'
+  brew install --cask orbstack
+  # OrbStack provides docker CLI compatibility out of the box
+elif is_linux; then
+  sudo apt install -y uidmap
 
-brew install podman
-brew install podman-compose
-brew install podman-tui
+  brew install podman
+  brew install podman-compose
+  brew install podman-tui
 
-PODMAN_DESKTOP_PACKAGE=io.podman_desktop.PodmanDesktop
+  PODMAN_DESKTOP_PACKAGE=io.podman_desktop.PodmanDesktop
 
-flatpak install -y --user flathub $PODMAN_DESKTOP_PACKAGE
-flatpak override --user --filesystem=/home/linuxbrew/.linuxbrew:ro $PODMAN_DESKTOP_PACKAGE
-flatpak override --user --env=PATH=/app/bin:/usr/bin:/home/linuxbrew/.linuxbrew/bin $PODMAN_DESKTOP_PACKAGE
+  flatpak install -y --user flathub $PODMAN_DESKTOP_PACKAGE
+  flatpak override --user --filesystem=/home/linuxbrew/.linuxbrew:ro $PODMAN_DESKTOP_PACKAGE
+  flatpak override --user --env=PATH=/app/bin:/usr/bin:/home/linuxbrew/.linuxbrew/bin $PODMAN_DESKTOP_PACKAGE
 
-create_symlink "$(brew --prefix podman)/bin/podman" "$HOME/.local/bin/docker"
-create_symlink "$(brew --prefix podman-compose)/bin/podman-compose" "$HOME/.local/bin/docker-compose"
+  create_symlink "$(brew --prefix podman)/bin/podman" "$HOME/.local/bin/docker"
+  create_symlink "$(brew --prefix podman-compose)/bin/podman-compose" "$HOME/.local/bin/docker-compose"
+fi
