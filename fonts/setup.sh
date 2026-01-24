72f5a6eedaafda394db772eef6a2f48829fd7e0f
Fiuza <fiuzagr@gmail.com>
2026-01-24 16:09:44 -0300

    Update fonts for macOS

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env sh
 
+if is_macos; then
+  FONTS_DIR="$HOME/Library/Fonts"
+else
+  FONTS_DIR="$HOME/.local/share/fonts"
+fi
+
+mkdir -p "$FONTS_DIR"
+
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip -O /tmp/FiraCode.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
 
-unzip -o /tmp/FiraCode.zip -d "$HOME/.local/share/fonts/FiraCode/"
-unzip -o /tmp/JetBrainsMono.zip -d "$HOME/.local/share/fonts/JetBrainsMono/"
+unzip -o /tmp/FiraCode.zip -d "$FONTS_DIR/FiraCode/"
+unzip -o /tmp/JetBrainsMono.zip -d "$FONTS_DIR/JetBrainsMono/"
 
-fc-cache -fv
+if is_linux; then
+  fc-cache -fv
+fi
