49db8e45812a5f24dbc565968a0b274db1c2d10f
Fiuza <fiuzagr@gmail.com>
2026-01-24 16:09:41 -0300

    Update homebrew paths

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env sh
 
-NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+get_brew_prefix() {
+  if is_macos; then
+    if [ -d "/opt/homebrew" ]; then
+      echo "/opt/homebrew"
+    else
+      echo "/usr/local"
+    fi
+  else
+    echo "/home/linuxbrew/.linuxbrew"
+  fi
+}
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+BREW_PREFIX=$(get_brew_prefix)
+
+if ! command -v brew >/dev/null 2>&1; then
+  NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+eval "$($BREW_PREFIX/bin/brew shellenv)"
 
 to_dotfilesrc '. "$DOTFILES_PATH/homebrew/env"'
